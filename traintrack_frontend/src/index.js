d514cca7b3f6f9424d18c72f15a04193bdd3d7c8
Jake <jtbartlow@gmail.com>
2019-05-31 11:42:50 -0500

    fri morning

--- traintrack_frontend/src/index.js
+++ traintrack_frontend/src/index.js
@@ -5,6 +5,7 @@ stationContainer.setAttribute("id", "stationContainer");
 const pageBody = document.querySelector("body");
 pageBody.append(stationContainer);
 let stationMarker = document.querySelectorAll("div");
+let allStationData = [];
 
 //Fetches initial station data and sends it to getUnique //
 function fetchData() {
@@ -17,26 +18,13 @@ function fetchData() {
     .then(parsedJson => getUnique(parsedJson));
 }
 
-// arrT - arrival time;
-// staId - station id
-// staNm - station name
-// isApp - approaching?
-// rt - route
-// http://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=4ba28f6b2b8843bf9cef1c0fcc05f874&rt=red&outputType=JSON
-
 //Removes duplicate station names and sends to fetchNewData
-
 function getUnique(arr, station_descriptive_name) {
   const unique = arr
     .map(e => e["station_descriptive_name"])
-
-    // store the keys of the unique objects
     .map((e, i, final) => final.indexOf(e) === i && i)
-
-    // eliminate the dead keys & store unique objects
     .filter(e => arr[e])
     .map(e => arr[e]);
-
   fetchNewData(unique);
 }
 
@@ -45,32 +33,48 @@ function fetchNewData(array) {
   for (var i = 0; i < array.length; i++) {
     var station_name = fetchProperty(array[i], "station_descriptive_name");
     var map_id = fetchProperty(array[i], "map_id");
-
+    var blue = fetchProperty(array[i], "blue");
+    var brown = fetchProperty(array[i], "brn");
+    var green = fetchProperty(array[i], "g");
+    var orange = fetchProperty(array[i], "o");
+    var purple = fetchProperty(array[i], "p");
+    var purpleExpress = fetchProperty(array[i], "pexp");
+    var pink = fetchProperty(array[i], "pnk");
+    var red = fetchProperty(array[i], "red");
+    var yellow = fetchProperty(array[i], "y");
     var tempJSON = {};
     tempJSON.station_descriptive_name = station_name;
     tempJSON.map_id = map_id;
-
+    tempJSON.blue = blue;
+    tempJSON.brn = brown;
+    tempJSON.g = green;
+    tempJSON.o = orange;
+    tempJSON.p = purple;
+    tempJSON.pexp = purpleExpress;
+    tempJSON.pnk = pink;
+    tempJSON.red = red;
+    tempJSON.y = yellow;
     newData.push(tempJSON);
   }
+  allStationData = newData;
+
   renderStations(newData);
 }
 fetchData();
 
 //Renders list of stations //
-function renderStations(newData) {
-  for (let station of newData) {
+function renderStations(arr) {
+  for (let station of arr) {
     let station_div = document.createElement("div");
     station_div.class = station;
     station_div.setAttribute("id", station.map_id);
     station_div.innerHTML = station.station_descriptive_name;
-    if (station_div.innerHTML.includes("blue")) {
-      station_div.style.color = "blue";
-    }
+
     station_div.addEventListener(
       "click",
       event => grabArrivals(event, station),
       {
-        // once: true
+        once: true
       }
     );
     stationContainer.append(station_div);
@@ -100,15 +104,15 @@ function showArrivals(arrivals) {
   let arrivalCard = document.createElement("ul");
   arrivalCard.setAttribute("class", "arrivalCard");
   for (arrival of arrivals.ctatt.eta) {
-    let arrivalListing = document.createElement("li");
+    let arrivalListing = document.createElement("p");
     let currentDate = new Date();
     let currentTime = currentDate.getTime();
     let etaDate = new Date(arrival.arrT);
     let etaTime = etaDate.getTime();
     let arrivalTime = Math.round((etaTime - currentTime) / 1000 / 60);
 
-    arrivalListing.innerHTML = `Destination: ${arrival.destNm} ETA: ${
-      arrival.isApp == 1
+    arrivalListing.innerHTML = `To ${arrival.destNm} | ETA: ${
+      arrivalTime >= 0 && arrivalTime <= 2
         ? "Approaching"
         : arrivalTime + " " + "minutes" && arrivalTime < 0
         ? "Delayed"
@@ -125,9 +129,9 @@ function showArrivals(arrivals) {
     .then(comments =>
       comments.forEach(comment => {
         if (comment.stationNum == stationNumber) {
-          let commentLi = document.createElement("li");
+          let commentLi = document.createElement("p");
           commentLi.innerHTML = comment.content;
-          commentContainer.style.border = "thick solid #0000FF";
+
           commentContainer.append(commentLi);
           arrivalCard.append(commentContainer);
         }
@@ -177,10 +181,10 @@ function submitComment(event, commentInput, stationNumber) {
       stationNum: stationNumber
     })
   });
-  let newComment = document.createElement("li");
+  let newComment = document.createElement("p");
   newComment.innerHTML = commentInput.value;
-  event.target.parentElement.append(newComment);
-  debugger;
+  event.target.nextSibling.append(newComment);
+
   event.target.reset();
 }
 //lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=ba92028c4ac34d1c89d827de312bb41d&max=6&mapid=40360&outputType=JSON
@@ -200,17 +204,74 @@ function filterAll(c) {
   }
 }
 function filterOrange(c) {
-  let orangeLine = [];
-  let allStations = Array.from(stationContainer.childNodes);
-  debugger;
-  let orangeStations = allStations.filter(station =>
-    station.innerHTML.includes("orange")
+  renderOrangeStations(allStationData);
+}
+
+function filterPink(c) {
+  renderPinkStations(allStationData);
+}
+
+function filterGreen(c) {
+  renderGreenStations(allStationData);
+}
+function filterRed(c) {
+  renderRedStations(allStationData);
+}
+function filterBrown(c) {
+  renderBrownStations(allStationData);
+}
+function filterPurple(c) {
+  renderPurpleStations(allStationData);
+}
+function filterBlue(c) {
+  renderBlueStations(allStationData);
+}
+function filterYellow(c) {
+  renderYellowStations(allStationData);
+}
+
+function renderOrangeStations() {
+  let orangeTrains = allStationData.filter(station => station.o);
+  stationContainer.innerHTML = "";
+  renderStations(orangeTrains);
+}
+function renderPinkStations() {
+  let pinkTrains = allStationData.filter(station => station.pnk);
+  stationContainer.innerHTML = "";
+  renderStations(pinkTrains);
+}
+function renderGreenStations() {
+  let greenTrains = allStationData.filter(station => station.g);
+  stationContainer.innerHTML = "";
+  renderStations(greenTrains);
+}
+
+function renderRedStations() {
+  let redTrains = allStationData.filter(station => station.red);
+  stationContainer.innerHTML = "";
+  renderStations(redTrains);
+}
+function renderBrownStations() {
+  let brownTrains = allStationData.filter(station => station.brn);
+  stationContainer.innerHTML = "";
+  renderStations(brownTrains);
+}
+function renderPurpleStations() {
+  let purpleTrains = allStationData.filter(
+    station => station.p || station.pexp
   );
-  // allStations.forEach(childNode => {
-  //   if (childNode.innerText.includes("orange")) {
-  //     orangeLine.push(childNode.id);
-  //   }
-  // });
+  stationContainer.innerHTML = "";
+  renderStations(purpleTrains);
+}
+function renderBlueStations() {
+  let blueTrains = allStationData.filter(station => station.blue);
+  stationContainer.innerHTML = "";
+  renderStations(blueTrains);
+}
+function renderYellowStations() {
+  let yellowTrains = allStationData.filter(station => station.y);
+  stationContainer.innerHTML = "";
+  renderStations(yellowTrains);
 }
 
 // Event Listeners//
